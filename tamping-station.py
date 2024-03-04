0592e179632b7a9b2c9d98c1cd66b13c84d2ce83
Felix Na <felix.na70@gmail.com>
2024-03-04 19:45:20 +0100

    add handle support

--- tamping-station.py
+++ tamping-station.py
@@ -4,6 +4,7 @@ from build123d import *
 
 ts_inner_dia = 75.0
 ts_outer_dia = 85.0
+ts_handle_support_offset = ts_outer_dia - ts_inner_dia
 ts_height = 79.0
 wing_width = 28.0
 wing_nut_depth = 7.5
@@ -13,10 +14,19 @@ handle_z_offset = 31.0
 handle_center_height = ts_height - wing_nut_depth - handle_z_offset + handle_dia/2
 
 with BuildPart() as ts:
+    # main body
     with BuildSketch() as sk1:
         Circle(ts_outer_dia / 2)
         Circle(ts_inner_dia / 2, mode=Mode.SUBTRACT)
     extrude(amount=ts_height)
+    with BuildSketch() as sk1:
+        Circle((ts_outer_dia + ts_handle_support_offset) / 2)
+        Circle(ts_inner_dia / 2, mode=Mode.SUBTRACT)
+        Rectangle(ts_outer_dia + ts_handle_support_offset, 
+                  handle_dia + ts_handle_support_offset, 
+                  mode=Mode.INTERSECT,
+                  align=(Align.MIN, Align.CENTER))
+    extrude(amount=handle_center_height)
     with BuildSketch(ts.faces().sort_by(Axis.Z)[-1]):
         Rectangle(wing_width, ts_outer_dia)
     extrude(amount=-wing_nut_depth, mode=Mode.SUBTRACT)
