2fc1fa790f4397207a687782bd18cbeefbae645d
Felix Na <felix.na70@gmail.com>
2024-03-07 20:10:54 +0100

    polish

--- tamping-station.py
+++ tamping-station.py
@@ -1,5 +1,13 @@
 from math import sqrt
-from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults
+from ocp_vscode import (
+    show,
+    show_object,
+    reset_show,
+    set_port,
+    set_defaults,
+    get_defaults,
+)
+
 set_port(3939)
 from build123d import *
 
@@ -12,7 +20,7 @@ ear_nut_depth = 0.0
 handle_dia = 18.0
 handle_z_offset = 31.0
 
-handle_center_height = ts_height - ear_nut_depth - handle_z_offset + handle_dia/2
+handle_center_height = ts_height - ear_nut_depth - handle_z_offset + handle_dia / 2
 
 with BuildPart() as ts:
     # main body
@@ -23,19 +31,18 @@ with BuildPart() as ts:
 
     # handle support
     with BuildSketch() as hs_sk:
-        r = 10.0 # handle_support_offset / 2
-        x = ts_outer_dia/2 + handle_support_offset - r
-        y = handle_dia/2
-        oc = Circle(ts_outer_dia/2)
+        r = 10.0
+        x = ts_outer_dia / 2 + handle_support_offset - r
+        y = handle_dia / 2
+        oc = Circle(ts_outer_dia / 2)
         with Locations((x, -y), (x, +y)):
             Circle(radius=r)
         make_hull(hs_sk.edges())
         Circle(ts_inner_dia / 2, mode=Mode.SUBTRACT)
     extrude(amount=ts_height)
 
-
     # portafilter wings
-    if (ear_nut_depth > 0.0):
+    if ear_nut_depth > 0.0:
         with BuildSketch(ts.faces().sort_by(Axis.Z)[-1]):
             Rectangle(ear_width, ts_outer_dia)
         extrude(amount=-ear_nut_depth, mode=Mode.SUBTRACT)
@@ -45,33 +52,38 @@ with BuildPart() as ts:
         with BuildLine():
             top_w = 13.0
             len = 15.0
-            l1 = Line((ts_outer_dia/2, ts_height), (ts_outer_dia/2, ts_height-top_w))
-            l1b = JernArc(l1@1, l1%1, r, arc_size=45)
-            l2 = PolarLine(l1b@1, len, -45.0)
-            l3 = Line(l2 @ 1, ((l2 @ 1).X, ts_height))
-            l4 = Line(l3@1, l1@0)
+            l1 = Line(
+                (ts_outer_dia / 2, ts_height), (ts_outer_dia / 2, ts_height - top_w)
+            )
+            l2 = JernArc(l1 @ 1, l1 % 1, r, arc_size=45)
+            l3 = PolarLine(l2 @ 1, len, -45.0)
+            l4 = Line(l3 @ 1, ((l3 @ 1).X, ts_height))
+            l5 = Line(l4 @ 1, l1 @ 0)
         make_face()
     revolve(axis=Axis.Z, mode=Mode.SUBTRACT)
 
     # slot for the handle
     if True:
         with BuildSketch(Plane.YZ):
-            with Locations((0,handle_center_height)):
-                Circle(handle_dia/2)
+            with Locations((0, handle_center_height)):
+                Circle(handle_dia / 2)
                 Rectangle(handle_dia, ts_height, align=(Align.CENTER, Align.MIN))
         extrude(amount=ts_outer_dia, mode=Mode.SUBTRACT)
 
-    sel = ts.edges().filter_by(GeomType.HYPERBOLA) + ts.edges().filter_by(GeomType.BSPLINE)
-    sel1 = sel.filter_by_position(Axis.Y, minimum=-ts_inner_dia/2, maximum=-handle_dia/2-1.0) + sel.filter_by_position(Axis.Y, minimum=handle_dia/2+.1, maximum=ts_inner_dia/2)
+    sel = ts.edges().filter_by(GeomType.HYPERBOLA) + ts.edges().filter_by(
+        GeomType.BSPLINE
+    )
+    sel1 = sel.filter_by_position(
+        Axis.Y, minimum=-ts_inner_dia / 2, maximum=-handle_dia / 2 - 1.0
+    ) + sel.filter_by_position(
+        Axis.Y, minimum=handle_dia / 2 + 0.1, maximum=ts_inner_dia / 2
+    )
 
-
-    sel2 = ts.edges().filter_by_position(Axis.Y,
-                                         minimum=-handle_dia/2,
-                                         maximum=handle_dia/2
-                                         ).filter_by_position(Axis.Z,
-                                                              minimum=ts_height/2,
-                                                              maximum=ts_height
-                                                              ) 
+    sel2 = (
+        ts.edges()
+        .filter_by_position(Axis.Y, minimum=-handle_dia / 2, maximum=handle_dia / 2)
+        .filter_by_position(Axis.Z, minimum=ts_height / 2, maximum=ts_height)
+    )
     fillet(sel1, radius=5.5)
     fillet(sel2, radius=0.6)
 
