5d65bedcaea88f32c3454ad60fc45783d42f2d4c
Felix Na <felix.na70@gmail.com>
2024-03-07 19:45:33 +0100

    symmetric fillet

--- tamping-station.py
+++ tamping-station.py
@@ -5,7 +5,7 @@ from build123d import *
 
 ts_inner_dia = 75.0
 ts_outer_dia = 85.0
-handle_support_offset = 10.0
+handle_support_offset = 9.0
 ts_height = 79.0
 ear_width = 28.0
 ear_nut_depth = 0.0
@@ -13,15 +13,6 @@ handle_dia = 18.0
 handle_z_offset = 31.0
 
 handle_center_height = ts_height - ear_nut_depth - handle_z_offset + handle_dia/2
-
-
-def midround(p1, r):
-    (x1,y1) = p1
-    l1 = sqrt(x1**2 + y1**2)
-    l2 = l1 - r
-    s = l2 / l1
-    p2 = (x1*s, y1*s)
-    return p2
 
 with BuildPart() as ts:
     # main body
@@ -33,16 +24,11 @@ with BuildPart() as ts:
     # handle support
     with BuildSketch() as hs_sk:
         r = 10.0 # handle_support_offset / 2
-        x = ts_outer_dia/2 + handle_support_offset -3
+        x = ts_outer_dia/2 + handle_support_offset - r
         y = handle_dia/2
         oc = Circle(ts_outer_dia/2)
-        with Locations(midround((x, -y), r)):
+        with Locations((x, -y), (x, +y)):
             Circle(radius=r)
-        with Locations(midround((x, +y), r)):
-            Circle(radius=r)
-#        with BuildLine() as bl:
-#            l = ThreePointArc((x, -y), (sqrt(x*x+y*y) ,0), (x, y))
-#            Line(l@0, l@1)
         make_hull(hs_sk.edges())
         Circle(ts_inner_dia / 2, mode=Mode.SUBTRACT)
     extrude(amount=ts_height)
@@ -57,7 +43,7 @@ with BuildPart() as ts:
     # chamfer the handle support
     with BuildSketch(Plane.XZ) as hs2_sk:
         with BuildLine():
-            top_w = 10.05
+            top_w = 11.0
             len = 15.0
             l1 = Line((ts_outer_dia/2, ts_height), (ts_outer_dia/2, ts_height-top_w))
             l1b = JernArc(l1@1, l1%1, r, arc_size=45)
@@ -75,13 +61,13 @@ with BuildPart() as ts:
                 Rectangle(handle_dia, ts_height, align=(Align.CENTER, Align.MIN))
         extrude(amount=ts_outer_dia, mode=Mode.SUBTRACT)
 
-    sel = ts.edges().filter_by(GeomType.HYPERBOLA) + ts.edges().filter_by(GeomType.BSPLINE)
-    sel1 = sel.filter_by_position(Axis.Y, minimum=-ts_inner_dia/2, maximum=-handle_dia/2-1.0)
-    sel2 = sel.filter_by_position(Axis.Y, minimum=handle_dia/2+.1, maximum=ts_inner_dia/2)
+    egs = ts.edges().filter_by(GeomType.HYPERBOLA) + ts.edges().filter_by(GeomType.BSPLINE)
+    egs_l = egs.filter_by_position(Axis.Y, minimum=-ts_inner_dia/2, maximum=-handle_dia/2-1.0)
+    egs_r = egs.filter_by_position(Axis.Y, minimum=handle_dia/2+.1, maximum=ts_inner_dia/2)
 
-    fillet(sel1, radius=5.0)
-#    fillet(sel2, radius=5.0)
+    fillet(egs_l, radius=5.0)
+    fillet(egs_r, radius=5.0)
 
-show(ts, sel1, sel2)
+show(ts, egs_l, egs_r)
 
 ts.part.export_step("tamping-station.step")
