4f5acdba2431f7945c6424cc2742b975489302cb
Felix Na <felix.na70@gmail.com>
2024-03-08 17:38:13 +0100

    simplify further

--- tamping-station.py
+++ tamping-station.py
@@ -4,41 +4,29 @@ from build123d import *
 
 ts_inner_dia = 75.0
 ts_outer_dia = 85.0
-handle_support_offset = 8.0
 ts_height = 75.0
 handle_dia = 18.0
 handle_z_offset = 33.0
 
-handle_center_height = ts_height - handle_z_offset + handle_dia / 2
-
 with BuildPart() as ts:
-    # main body
-    with BuildSketch() as sk1:
-        Circle(ts_outer_dia / 2)
-        Circle(ts_inner_dia / 2, mode=Mode.SUBTRACT)
-    extrude(amount=ts_height)
-
     # handle support
     with BuildSketch() as hs_sk:
-        r = 10.0
-        x = ts_outer_dia / 2 + handle_support_offset - r
+        x = ts_outer_dia / 2 - 2.0
         y = handle_dia / 2
-        oc = Circle(ts_outer_dia / 2)
+        Circle(ts_outer_dia / 2)
         with Locations((x, -y), (x, +y)):
-            Circle(radius=r)
+            Circle(10.0)
         make_hull(hs_sk.edges())
         Circle(ts_inner_dia / 2, mode=Mode.SUBTRACT)
     extrude(amount=ts_height)
 
     # chamfer the handle support
-    with BuildSketch(Plane.XZ) as hs2_sk:
+    with BuildSketch(Plane.XZ) as hsc_sk:
         with BuildLine():
-            top_w = 13.0
-            len = 15.0
             l1 = Line((ts_outer_dia / 2, ts_height), 
-                      (ts_outer_dia / 2, ts_height - top_w))
-            l2 = JernArc(l1 @ 1, l1 % 1, r, arc_size=45)
-            l3 = PolarLine(l2 @ 1, len, -45.0)
+                      (ts_outer_dia / 2, ts_height - 13.0))
+            l2 = JernArc(l1 @ 1, l1 % 1, 10.0, arc_size=45)
+            l3 = PolarLine(l2 @ 1, 15.0, -45.0)
             l4 = Line(l3 @ 1, ((l3 @ 1).X, ts_height))
             l5 = Line(l4 @ 1, l1 @ 0)
         make_face()
@@ -46,12 +34,15 @@ with BuildPart() as ts:
 
     # slot for the handle
     with BuildSketch(Plane.YZ):
-        with Locations((0, handle_center_height)):
+        with Locations((0, ts_height - handle_z_offset + handle_dia / 2)):
             Circle(handle_dia / 2)
-            Rectangle(handle_dia, ts_height, align=(Align.CENTER, Align.MIN))
+            Rectangle(handle_dia, 
+                      ts_height, 
+                      align=(Align.CENTER, Align.MIN))
     extrude(amount=ts_outer_dia, mode=Mode.SUBTRACT)
 
-    sel = ts.edges().filter_by(GeomType.HYPERBOLA) + ts.edges().filter_by(GeomType.BSPLINE)
+    sel = (ts.edges().filter_by(GeomType.HYPERBOLA) +
+           ts.edges().filter_by(GeomType.BSPLINE))
 
     sel1 = sel.filter_by_position(Axis.Y,
                                   minimum=-ts_inner_dia / 2,
